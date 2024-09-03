580f42a5104341e6001382e38e9c566557f8d4ac
Muhammed Salih Guler <muhammedsalihguler@gmail.com>
2024-09-03 10:52:14 +0200

    Update user_information.dart

--- codes/profile/user_information.dart
+++ codes/profile/user_information.dart
@@ -1,1 +1,30 @@
+class UserInformation {
+  const UserInformation({
+    required this.id,
+    required this.email,
+    required this.username,
+    required this.fullName,
+  });
 
+  final String id;
+  final String email;
+  final String username;
+  final String fullName;
+
+  @override
+  int get hashCode => Object.hashAll([
+        id,
+        email,
+        username,
+        fullName,
+      ]);
+
+  @override
+  bool operator ==(Object other) {
+    return other is UserInformation &&
+        (id == other.id) &&
+        (email == other.email) &&
+        (username == other.username) &&
+        (fullName == other.fullName);
+  }
+}
